397e055ee76fbc886e7e5958cc6bfebb33cece75
surainfernando <59121315+surainfernando@users.noreply.github.com>
2020-02-24 18:38:13 +0530

    Basic Login Storyboad UI

---
+++ NIBM-Event-App/ViewControllers/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  NIBM-Event-App
+//
+//  Created by user161121 on 2/24/20.
+//  Copyright © 2020 surainfdo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
