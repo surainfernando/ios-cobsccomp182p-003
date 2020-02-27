0fce72f08a51f06775108ed431e17a8d38bfa12d
surainfernando <59121315+surainfernando@users.noreply.github.com>
2020-02-27 09:01:09 +0530

    Create event page ui version 2

--- NIBM-Event-App/ViewControllers/EventViewController.swift
+++ NIBM-Event-App/ViewControllers/EventViewController.swift
@@ -10,8 +10,28 @@ import UIKit
 
 class EventViewController: UIViewController {
 
-    @IBOutlet weak var imageview1: UIImageView!
-    @IBOutlet weak var imageview2: UIImageView!
+    
+    @IBOutlet weak var TitleTextView: UITextField!
+    
+    @IBOutlet weak var DescriptionTextView: UITextView!
+    
+    @IBOutlet weak var DatePicker: UIDatePicker!
+    
+    
+    @IBOutlet weak var StartTimePicker: UIDatePicker!
+    
+    
+    @IBOutlet weak var EndTimePicker: UIDatePicker!
+    
+    
+    @IBOutlet weak var ContactnoTextField: UITextField!
+    
+    @IBOutlet weak var PhotoImageView: UIImageView!
+    
+    
+    
+    
+    
     var imagePicker: ImagePicker!
     
     override func viewDidLoad() {
@@ -22,52 +42,53 @@ class EventViewController: UIViewController {
     }
     
     
-    @IBAction func pickimage1(_ sender: UIButton) {
-        self.imagePicker.present(from: sender)
+    @IBAction func createEventAction(_ sender: Any) {
+        
     }
     
-    @IBAction func convertDiaplay(_ sender: Any) {
-        convertImageToBase64()
-    }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
     
-    func convertImageToBase64()
-    {
-        guard let  image = imageview1.image else{
-            return
-        }
-       
-     guard  let jpegData = image.jpegData(compressionQuality: 1.0) else //converted to base 64
-       {return
-        
-        }
-        let strBase64 = jpegData.base64EncodedString(options: .lineLength64Characters)
-        let nsd: NSData = NSData(base64Encoded: strBase64, options: NSData.Base64DecodingOptions.ignoreUnknownCharacters)!
-        if let image = UIImage(data: nsd as Data) {
-            imageview2.image=image
-        }
-        
-        
-        
-  
-        
-        
-        
-    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    func convertImageToBase64()
+//    {
+//        guard let  image = imageview1.image else{
+//            return
+//        }
+//
+//     guard  let jpegData = image.jpegData(compressionQuality: 1.0) else //converted to base 64
+//       {return
+//
+//        }
+//        let strBase64 = jpegData.base64EncodedString(options: .lineLength64Characters)
+//        let nsd: NSData = NSData(base64Encoded: strBase64, options: NSData.Base64DecodingOptions.ignoreUnknownCharacters)!
+//        if let image = UIImage(data: nsd as Data) {
+//            imageview2.image=image
+//        }
+//
+//
+//
+//
+//
+//
+//
+//    }
 
 }
 extension EventViewController: ImagePickerDelegate {
     
     func didSelect(image: UIImage?) {
-        self.imageview1.image = image
+        self.PhotoImageView.image = image
     }
 }
